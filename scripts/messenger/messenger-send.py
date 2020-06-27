8079da76a67079039560fa1bbfc7b2106c6efc06
tak289 <50550259+tak289@users.noreply.github.com>
2020-06-27 18:14:56 -0400

    #212 - New messenger script in python for tbs

---
+++ scripts/messenger/messenger-send.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+##
+## Messenger app - sends some notification to mobile phone
+## Uses Telegram. TD 06/2020
+##
+## Requires module pyTelegramBotAPI
+##
+
+import logging
+import json
+import os.path
+import argparse
+
+##
+## Logging
+##
+
+logging.basicConfig(level=logging.DEBUG)
+
+def setup_logger():
+	global logger
+	# Remove default handler
+	logger = logging.getLogger()
+	while logger.hasHandlers():
+		logger.removeHandler(logger.handlers[0])
+
+	logger = logging.getLogger("messenger")
+
+	formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+	ch = logging.StreamHandler()
+	ch.setFormatter(formatter)
+	ch.setLevel(logging.DEBUG)
+	logger.addHandler(ch)
+setup_logger()
+
+##
+## Default variables & Config
+##
+default_configfile = '/etc/tbs/tbs.conf'
+
+## Argument parsing
+
+parser = argparse.ArgumentParser(prog='messenger-send', usage='%(prog)s [options]', description='Sending messages with Telegram.')
+parser.add_argument('-c', '--configfile', dest='configfile', default=default_configfile, metavar='FILE', type=str, help='Path to configfile')
+parser.add_argument('-r', '--receipient', dest='receipient', required=True, metavar='MESSENGER_ID', type=str, help='Receipient number')
+parser.add_argument('-m', '--message', dest='message', required=True, metavar='MESSAGE', type=str, help='The message to be sent.')
+args = parser.parse_args()
+
+###############
+
+# Parse config file
+
+configfile = args.configfile
+logger.debug("Using config file " + configfile)
+if not os.path.isfile(configfile):
+	logger.critical('Config file not found: ' + configfile)
+
+with open(configfile) as config_file:
+    config = json.load(config_file)
+
+messenger = config['messenger']
+
+###############
+# Send messages
+# https://github.com/eternnoir/pyTelegramBotAPI/blob/master/telebot/apihelper.py
+
+import telebot
+
+token = messenger["bot_id"] + ":" + messenger["api_key"]
+logger.debug(f"Creating bot instance with token: {token}")
+bot = telebot.TeleBot(token)
+
+receipient = args.receipient
+message = args.message
+
+# Lookup for known numbers in config file
+if receipient in messenger["receipients"]:
+	logger.debug(f"Replacing {receipient} with {messenger['receipients'][receipient]}")
+	receipient = messenger["receipients"][receipient]
+
+logger.debug(f"Sending message to {receipient}: {message}")
+bot.send_message(receipient, message)
